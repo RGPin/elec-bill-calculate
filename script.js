5e82ba42bde05dc32a85c1641e19eff6d13e39a0
RGPin <157803400+RGPin@users.noreply.github.com>
2026-01-11 12:00:15 +0800

    Automate data retrieval, get consumption of each room, get total consumption, get electric bill

--- script.js
+++ script.js
@@ -5,33 +5,24 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    const currInputs = document.querySelectorAll('input[name$="-curr"]');
     const currReadings = {};
     for (let i = 1; i <=6; i++) {
-        currReadings[`room${i}`] = Number(formData.get(`room${i}-curr`));
+        currReadings[i] = Number(formData.get(`room${i}-curr`));
     }
 
-    console.log(currReadings)
+    const prevReadings = {};
+    for (let i = 1; i <=6; i++) {
+        prevReadings[i] = Number(formData.get(`room${i}-prev`));
+    }
 
-    // const room1Prev = Number(formData.get("room1-prev"));
-    // const room2Prev = Number(formData.get("room2-prev"));
-    // const room3Prev = Number(formData.get("room3-prev"));
-    // const room4Prev = Number(formData.get("room4-prev"));
-    // const room5Prev = Number(formData.get("room5-prev"));
-    // const room6Prev = Number(formData.get("room6-prev"));
+    const consumption = {};
 
-    // const room1Curr = Number(formData.get("room1-curr"));
-    // const room2Curr = Number(formData.get("room2-curr"));
-    // const room3Curr = Number(formData.get("room3-curr"));
-    // const room4Curr = Number(formData.get("room4-curr"));
-    // const room5Curr = Number(formData.get("room5-curr"));
-    // const room6Curr = Number(formData.get("room6-curr"));
+    for (const room in currReadings) {
+        consumption[room] = currReadings[room] - prevReadings[room];
+    }
 
-    // const room1Consumption = room1Curr - room1Prev;
-    // const room2Consumption = room2Curr - room2Prev;
-    // const room3Consumption = room3Curr - room3Prev;
-    // const room4Consumption = room4Curr - room4Prev;
-    // const room5Consumption = room5Curr - room5Prev;
-    // const room6Consumption = room6Curr - room6Prev;
+    const totalConsumption = Object.values(consumption).reduce((acc, curr) => (acc + curr), 0);
+    
+    const electricBill = Number(formData.get("bill"));
     
 });
